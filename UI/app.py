c0a44b5bd50b0f676087733f2ac4220e820331cd
Varun Pusarla <varunpusarla@gmail.com>
2023-07-06 23:53:08 +0530

    Speed improvements

--- UI/app.py
+++ UI/app.py
@@ -5,6 +5,9 @@ from paddleocr import PaddleOCR
 from ultralytics import YOLO
 import numpy as np
 from PIL import Image
+import concurrent.futures
+import asyncio
+
 
 # Initialize Tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
@@ -12,35 +15,69 @@ pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tessera
 # Initialize PaddleOCR
 ocr_paddle = PaddleOCR()
 
-# Function to perform OCR on the image
 def perform_ocr(image, model):
     # Perform object detection
     results = model.predict(image)
 
-    # Perform OCR with Tesseract on the cropped parts of the image
+    #list to store the OCR results
     ocr_tesseract_results = []
-    for result in results:
-        boxes = result.boxes.cpu().numpy()
-        for i, box in enumerate(boxes):
-            name = result.names[int(box.cls)]
-            r = box.xyxy[0].astype(int)
-            crop = image.crop((r[0], r[1], r[2], r[3]))
-            ocr_text = pytesseract.image_to_string(crop)
-            ocr_tesseract_results.append((name, ocr_text))
-
-    # Perform OCR with PaddleOCR on the cropped parts of the image
     ocr_paddle_results = []
-    for result in results:
+
+    async def perform_ocr_tesseract(crop, name):
+        # Perform OCR with Tesseract on the cropped image
+        ocr_text_tesseract = pytesseract.image_to_string(crop)
+        return (name, ocr_text_tesseract)
+
+    async def perform_ocr_paddle(crop, name):
+        # Perform OCR with PaddleOCR on the cropped image
+        ocr_result_paddle = ocr_paddle.ocr(np.array(crop))
+        ocr_text_paddle = '\n'.join([word_info[1][0] for line in ocr_result_paddle for word_info in line])
+        return (name, ocr_text_paddle)
+
+    async def process_result(result):
         boxes = result.boxes.cpu().numpy()
+        crops = []
+        names = []
         for i, box in enumerate(boxes):
             name = result.names[int(box.cls)]
             r = box.xyxy[0].astype(int)
             crop = image.crop((r[0], r[1], r[2], r[3]))
-            ocr_result = ocr_paddle.ocr(np.array(crop))
-            ocr_text = '\n'.join([word_info[1][0] for line in ocr_result for word_info in line])
-            ocr_paddle_results.append((name, ocr_text))
+            crops.append(crop)
+            names.append(name)
+
+        # Perform OCR with Tesseract concurrently
+        tesseract_tasks = [perform_ocr_tesseract(crop, name) for crop, name in zip(crops, names)]
+        tesseract_results = await asyncio.gather(*tesseract_tasks)
+
+        # Perform OCR with PaddleOCR concurrently
+        paddle_tasks = [perform_ocr_paddle(crop, name) for crop, name in zip(crops, names)]
+        paddle_results = await asyncio.gather(*paddle_tasks)
+
+        return tesseract_results, paddle_results
+
+    async def process_results(results):
+        return await asyncio.gather(*[process_result(result) for result in results])
+
+    # new event loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    try:
+        # Run the OCR processing asynchronously
+        ocr_results = loop.run_until_complete(process_results(results))
+
+        # Flatten the nested results
+        for tesseract_results, paddle_results in ocr_results:
+            ocr_tesseract_results.extend(tesseract_results)
+            ocr_paddle_results.extend(paddle_results)
+
+    finally:
+        # Close the event loop to clean up resources
+        loop.close()
 
     return ocr_tesseract_results, ocr_paddle_results
+
+
 
 def display_ocr_results(results):
     for result in results:
@@ -50,7 +87,7 @@ def display_ocr_results(results):
 
 # Set page configuration
 st.set_page_config(
-    page_title="OCR Process",
+    page_title="Invoice Process",
     layout="wide",
     initial_sidebar_state="expanded"
 )
@@ -83,12 +120,13 @@ st.markdown(
 )
 
 # Create the Streamlit app
-st.title("OCR Process")
+st.title("Invoice Processing")
 
 # Load the YOLO model
 @st.cache_resource
 def load_model():
     return YOLO('best.pt')
+    
 
 model = load_model()
 
@@ -102,31 +140,12 @@ if uploaded_file is not None:
 
     image = Image.open(uploaded_file)
 
-# Resize the image
     resized_image = image.resize((300, 300))
 
 # Display the resized image
     st.image(resized_image, caption="Uploaded Image")
 
-    # Display the uploaded image
-    # st.image(image, caption="Uploaded Image", use_column_width=True, width=200)
-
-    # Perform OCR on the uploaded image
     ocr_tesseract_results, ocr_paddle_results = perform_ocr(image, model)
-
-    # st.header("Tesseract OCR Results:")
-    # for result in ocr_tesseract_results:
-    #     name, ocr_text = result
-    #     st.write("**Object:**", name)
-    #     st.write("**OCR Text (Tesseract):**", ocr_text)
-    #     st.write("---")
-
-    # st.header("PaddleOCR Results:")
-    # for result in ocr_paddle_results:
-    #     name, ocr_text = result
-    #     st.write("**Object:**", name)
-    #     st.write("**OCR Text (PaddleOCR):**", ocr_text)
-    #     st.write("---")
 
     st.header("Tesseract OCR Results:")
     display_ocr_results(ocr_tesseract_results)
